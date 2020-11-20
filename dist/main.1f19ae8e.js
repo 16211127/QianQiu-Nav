d8a83a27c5881fce348800b365bba7da6ed3c7be
16211127 <15334629281@163.com>
2020-11-20 10:38:18 +0800

    v1.0.0.5 基本解决了手机和网页的兼容问题

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -209,7 +209,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64749" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65304" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
